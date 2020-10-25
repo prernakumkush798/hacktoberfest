05a6dbca9d8463992ad416522455a628801a3b0e
Rahul <70771921+rahulaxz@users.noreply.github.com>
2020-10-25 19:29:38 +0530

    Add files via upload

---
+++ callbyvalue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int callbyvalue(int &x,int &y)
+{
+    int z=x;
+    x=y;
+    y=z;
+}
+int main()
+{
+    int a=5,b=8;
+    cout<<"before swapping"<<a<<","<<b<<"\n";
+    callbyvalue( a, b);
+    cout<<"after swapping"<<a<<" " <<b;
+    return 0;
+
+}
